09d0ac5351a6f5a1b1482d82bb02414553299ac4
Lautaro Ignacio Castillo <castillolautaroig@gmail.com>
2023-09-30 18:03:28 -0300

    Finish exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = (input, timesToRepeat) => {
+  let inputAltered = input;
+  if (timesToRepeat < 0) {
+    return "ERROR";
+  }
+  if (timesToRepeat === 0) {
+    return "";
+  }
+  for (let i = 0; i < timesToRepeat - 1; i++) {
+    inputAltered += input;
+  }
+  return inputAltered;
 };
 
 // Do not edit below this line
