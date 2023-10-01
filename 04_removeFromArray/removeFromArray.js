bc878468498f3303391b4990ebb9fee6c72b75b8
Lautaro Ignacio Castillo <castillolautaroig@gmail.com>
2023-10-01 08:48:38 -0300

    Finish exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = (arr, ...argsToRemove) => {
+  const arrWithValuesRemoved = arr.filter((element) => {
+    if (!argsToRemove.includes(element)) {
+      return element;
+    }
+  })
+  return arrWithValuesRemoved;
 };
 
 // Do not edit below this line
